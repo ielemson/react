c61b59ac7a46442c2534934fc59df206d27bc63a
ielemson <ielemson@gmail.com>
2022-06-02 02:42:23 +0100

    added in progress toast for social login

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { FaGithub,FaTwitter,FaLinkedinIn } from "react-icons/fa";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 const Hero = () =>{
   return (
     
